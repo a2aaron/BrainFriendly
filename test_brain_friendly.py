04624c018dccd6f059bcc6a5f90200dbc28f077c
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-03-18 18:38:48 -0400

    Add extra tests for "[" and "]"
    Just some general extra tests. In particular, tests for starting with custom indexes or cell arrays have been added.

--- test_brain_friendly.py
+++ test_brain_friendly.py
@@ -38,9 +38,18 @@ def test_trivial_loops():
     assert eval_program('[][][][]', 0, [0]) == [0]
     assert eval_program('[[[][][[]]][][[][][[][]]][]]', 0, [0]) == [0]
 
-def test_increment_and_loop():
-    assert eval_program('+>[]', 0, [0,0]) == [1,0]
-    assert eval_program('[]>+', 0, [0,0]) == [0,1]
+def test_simple_loops():
+    assert eval_program('+++>[]', 0, [0,0]) == [3,0]
+    assert eval_program('[]>++', 0, [0,0]) == [0,2]
+
+    # "Copy value" program.
+    # Copies the current cell's value into the next two cells
+    assert eval_program('[>+>+<<-]', 1, [0,3,0,0]) == [0,0,3,3]
+
+    # "Move value" program.
+    # Moves the current cell two cells right.
+    assert eval_program('>>[-]<<[->>+<<]', 0, [5,0,0]) == [0,0,5]
+
 
 def test_bracket_jump():
     # If the value at the current cell is 0
@@ -55,6 +64,7 @@ def test_set_zero():
     # [+] and [-] should set the current cell to zero
     # Since there is under/overflow, this should always work.
     assert eval_program('[-]>[+]', 0, [-1,1]) == [0,0]
+    assert eval_program('[-]<[+]', 1, [100,-100]) == [0,0]
 
 def test_nested_loops():
     # Modified "hello world" program (without I/O). Taken from Esolang Wiki
@@ -63,7 +73,8 @@ def test_nested_loops():
     program = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]'
     assert eval_program(program, 0, [0]*7) == [0,0,72,104,88,32,8]
 
-    # "Move value" program. Taken from Esolang Wiki
-    # Moves the current cell two cells right.
-    assert eval_program('>>[-]<<[->>+<<]', 0, [1,0,0]) == [0,0,1]
+    # Slightly modified "multiplication" program. Taken from StackOverflow
+    # Takes two numbers in cell 0 and 1 and outputs their product in cell 2.
+    # http://stackoverflow.com/a/26708313
+    assert eval_program('[>[->+>+<<]>>[-<<+>>]<<<-]>[-]', 0, [3,5,0,0]) == [0,0,15,0]
 
