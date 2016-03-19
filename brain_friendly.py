e7376bc3fc2979f513b78b773e8ca140725d4d97
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-03-18 21:24:00 -0400

    Change eval_program() to use a brace dictionary.
    eval_program now uses get_brace_matches in "[" and "]" commands.

--- brain_friendly.py
+++ brain_friendly.py
@@ -1,5 +1,5 @@
 # The input BF program.
-bf_program_string = "++++++>----"
+bf_program_string = '[][]'
 # Current pointer location.
 bf_index = 0
 # Array of byte cells (currently 30,000).
@@ -8,7 +8,7 @@ bf_array = [0]*30000
 def eval_program(program, index, array, input=None, output=None):
     program_index = 0
     # Stack for "[" and "]" commands.
-    bracket_stack = list()
+    brace_dict = get_brace_matches(program)
     while program_index != len(program):
         command = program[program_index]
         if command == "+":
@@ -32,31 +32,20 @@ def eval_program(program, index, array, input=None, output=None):
                 # This sends 128 -> -128, and 255 -> -1
                 if array[index] > 127:
                     array[index] -= 256
+
         elif command == "[":
             # "[" jumps past the matching "[" if the current cell is 0.
-
-            # Pushing this is fine even if the current cell is 0
-            # Because it will be poped when we jump to the "]"
-            bracket_stack.append(program_index)
-
-            # Search for next "]"
             if array[index] == 0:
-                bracket_count = 0
-                while bracket_count != -1 :
-                    program_index += 1
-                    if program[program_index] == "[":
-                        bracket_count += 1
-                    elif program[program_index] == "]":
-                        bracket_count -= 1
-                    
+                    program_index = brace_dict[program_index]
+
         elif command == "]":
             # "]" jumps back to the matching "[" if the current cell is NOT 0
             if array[index] != 0:
                 #Go to matching "["
-                program_index = bracket_stack[-1]
-            else:
-                # Fall out of loop, pop matching "["
-                bracket_stack.pop()
+                for left, right in brace_dict.iteritems():
+                    if right == program_index:
+                        program_index = left
+
         # Next command
         program_index += 1                    
     return array
