23c284b70a0d526f08d2d571fa2be0d531636807
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-03-19 19:52:48 -0400

    Add style edits and a few refactors.
    Almost all of the changes here are minor style fixes or refactoring varible names. No major program flow alterations.

--- brain_friendly.py
+++ brain_friendly.py
@@ -3,48 +3,48 @@ bf_program_string = '[][]'
 # Current pointer location.
 bf_index = 0
 # Array of byte cells (currently 30,000).
-bf_array = [0]*30000
+bf_memory = [0]*30000
 
-def eval_program(program, index, array, input=None, output=None):
+
+def eval_program(program, index, memory, input=None, output=None):
     program_index = 0
     # Stack for "[" and "]" commands.
-    brace_dict = get_brace_matches(program)
-    while program_index != len(program):
+    brace_pairs = get_brace_matches(program)
+    while program_index < len(program):
         command = program[program_index]
-        if command == "+":
-            array[index] = increment_command(array[index])
-        elif command == "-":
-            array[index] = decrement_command(array[index])
-        elif command == ">":
-            if index+1 < len(array):
+        if command == '+':
+            memory[index] = increment_command(memory[index])
+        elif command == '-':
+            memory[index] = decrement_command(memory[index])
+        elif command == '>':
+            if index + 1 < len(memory):
                 index += 1
-        elif command == "<":
+        elif command == '<':
             if index > 0:
                 index -= 1
-        elif command == ".":
+        elif command == '.':
             if output:
                 # The output byte must be in range(256)
-                output.write(bytearray([array[index] % 256]))
+                output.write(bytearray([memory[index] % 256]))
         elif command == ',':
             if input:
-                array[index] = ord(input.read(1))
+                memory[index] = ord(input.read(1))
                 # The internal bytes are in the range [-128, 127].
                 # This sends 128 -> -128, and 255 -> -1
-                if array[index] > 127:
-                    array[index] -= 256
+                if memory[index] > 127:
+                    memory[index] -= 256
 
-        elif command == "[":
+        elif command == '[':
             # "[" jumps past the matching "[" if the current cell is 0.
-            if array[index] == 0:
-                    program_index = brace_dict[program_index]
+            if memory[index] == 0:
+                    program_index = brace_pairs[program_index]
 
-        elif command == "]":
+        elif command == ']':
             # "]" jumps back to the matching "[" if the current cell is NOT 0
-            if array[index] != 0:
-                program_index = brace_dict[program_index]
-        # Next command
-        program_index += 1                    
-    return array
+            if memory[index] != 0:
+                program_index = brace_pairs[program_index]
+        program_index += 1  # Next command
+    return memory
 
 
 def increment_command(value):
@@ -60,30 +60,31 @@ def decrement_command(value):
     else:
         return value - 1
 
+
 def get_brace_matches(program):
     '''
     Returns a dictionary containing the pairs for left and right brace indexes.
     If the program is malformed, then ValueError is raised.
     '''
-    brace_dict = dict()
-    brace_stack = list()
+    brace_pairs = {}
+    brace_stack = []
 
-    for index in range(len(program)):
-        if program[index] == "[":
+    for index, instruction in enumerate(program):
+        if instruction == '[':
             brace_stack.append(index)
-        elif program[index] == "]":
-            if len(brace_stack) == 0: #sanity check
+        elif instruction == ']':
+            if not brace_stack:  # Sanity check
                 raise ValueError("Malformed BF program (too many \"]\"s)")
             left_index = brace_stack.pop()
-            #add left AND right brace pairs.
-            brace_dict[left_index] = index
-            brace_dict[index] = left_index
+            # Add left AND right brace pairs.
+            brace_pairs[left_index] = index
+            brace_pairs[index] = left_index
 
-    if len(brace_stack) == 0: #sanity check
-        return brace_dict
-    else:
+    if brace_stack:  # Sanity check if brace_stack is empty
         raise ValueError("Malformed BF program (too many \"[\"s)")
+    else:
+        return brace_pairs
 
 if __name__ == '__main__':
-    bf_array = eval_program(bf_program_string, bf_index, bf_array)
-    print(bf_array[0:10])
+    bf_memory = eval_program(bf_program_string, bf_index, bf_memory)
+    print(bf_memory[:10])
